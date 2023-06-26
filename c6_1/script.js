ee009eada85fa2cf009228d1ac34c2488dfd1e7e
crusherakakola <crusherakakola@gmail.com>
2023-06-26 18:01:55 +0400

    c6_1 complete

---
+++ c6_1/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', () => {
+    document.querySelectorAll('.icon').forEach(element => {
+        element.classList.toggle('hidden')
+    });
+    
+})
